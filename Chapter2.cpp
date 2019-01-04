a685a3ed9e7d86beea6d48cffd5228237cecb5c6
YuBingZhou <43020665+YuBingZhou@users.noreply.github.com>
2019-01-04 19:23:21 +0800

    test

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -4,10 +4,12 @@ int main(int argc, char const *argv[]) {
 
   unsigned u = 10, u2 = 42;
   int i = 10, i2 = 42;
+  int &a = 10;
 
   std::cout << u2 - u << '\n';
   std::cout << u- u2 << '\n';
   std::cout << i - u << '\n';
   std::cout << u - i << '\n';
+  std::cout << a ;
   return 0;
 }
